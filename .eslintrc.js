0b2e0a4a25f8cff08990201190c3a49f81e19fdc
Jeffrey Chupp <jeff@semanticart.com>
2023-10-24 13:36:37 -0400

    Formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,43 +5,43 @@ module.exports = {
     jest: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended',
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
     // Make sure this is always the last configuration in the extends array.
-    'prettier',
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
-    'react/require-default-props': 'off', // default props are deprecated and shouldn't be used: https://sophiabits.com/blog/stop-using-defaultprops
-    'no-unused-vars': 'off', // this rule is incompatible with typescript-eslint's 'no-unused-vars
-    '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'react/jsx-filename-extension': [2, {extensions: ['.js', '.jsx', '.ts', '.tsx']}],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
+    "react/require-default-props": "off", // default props are deprecated and shouldn't be used: https://sophiabits.com/blog/stop-using-defaultprops
+    "no-unused-vars": "off", // this rule is incompatible with typescript-eslint's 'no-unused-vars
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
       {
-        ts: 'never',
-        tsx: 'never',
+        ts: "never",
+        tsx: "never",
       },
     ],
   },
   overrides: [
     {
-      files: ['*.test.ts', '*.test.tsx'],
+      files: ["*.test.ts", "*.test.tsx"],
       rules: {
-        'import/no-extraneous-dependencies': 'off',
+        "import/no-extraneous-dependencies": "off",
       },
     },
   ],
