a3b43f583dc2a712040aa42f4294fce2e02a3d1f
ayip8 <16192761+ayip8@users.noreply.github.com>
2023-12-11 09:20:19 -0500

    Fix linting issue

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ version=$(node -e "console.log(require('./package.json').version)")
 echo "Building version $version"
 
 echo "// THIS FILE IS GENERATED" >src/version.ts
-echo "export default '$version';" >>src/version.ts
+echo "export default \"$version\";" >>src/version.ts
 
 tsc
--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
 // THIS FILE IS GENERATED
-export default '0.1.19';
+export default "0.1.19";
