68ac7fe6595ef73f938a8e09d6761cd0f141daa9
Jeffrey Chupp <jeff@semanticart.com>
2025-03-11 14:25:09 -0400

    Support ESM

--- build.sh
+++ build.sh
@@ -10,4 +10,8 @@ echo "// THIS FILE IS GENERATED" >src/version.ts
 echo "export default \"$version\";" >>src/version.ts
 git add src/version.ts
 
+# Build CommonJS version
 tsc
+
+# Build ESM version
+tsc -p tsconfig.esm.json
