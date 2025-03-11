a4927bb0e2b329a9b157e05afd7894ccddf8c103
Jeffrey Chupp <jeff@semanticart.com>
2025-03-11 14:25:09 -0400

    Support ESM

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/jest.setup.ts"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+  moduleNameMapper: {
+    // Handle module aliases and resolve paths
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  // Support for ESM modules in Jest
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  transformIgnorePatterns: ["node_modules/(?!(@prefab-cloud/prefab-cloud-js)/)"],
+};
